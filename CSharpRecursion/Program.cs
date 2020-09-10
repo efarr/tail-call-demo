82502b448bed0c0655477b6ea076f1488ea2e02c
Eric Farr <efarr@aqnet.com>
2020-09-10 14:58:53 -0400

    Fibonocci sequence

---
+++ CSharpRecursion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSharpRecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fib(100000, 0, 1));
+        }
+
+        static long Fib(int n, long a, long b)
+        {
+            if (n == 0)
+                return a;
+            if (n == 1)
+                return b;
+
+            return Fib(n - 1, b, a + b);
+        }
+    }
+}
---
+++ FSharpRecursion/Program.fs
@@ -0,0 +1,10 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    let fib n =
+        let rec rfib i p2 p1 =
+            if i = n then p1
+            else rfib (i+1) p1 (p2+p1)
+        rfib 1 0 1
+    fib 100000
